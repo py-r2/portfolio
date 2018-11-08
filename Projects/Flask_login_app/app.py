25ae91d2f331e907bb60d7412b11f340ae7e5ea2
Romeo Rauta <35073991+py-r2@users.noreply.github.com>
2018-11-07 19:41:17 -0700

    added comments and cleaned up code

--- Projects/Flask_login_app/app.py
+++ Projects/Flask_login_app/app.py
@@ -16,63 +16,69 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-#define database model
-#implement a user class easier by inheriting from UserMixin user class properties and methods
-class Data(UserMixin, db.Model):
+#define database model by inheriting from UserMixin user class properties and methods
+class storeUserData(UserMixin, db.Model):
+    __tablename__="userdata"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     password = db.Column(db.String(80))
     email = db.Column(db.String(50), unique=True)
     company = db.Column(db.String(120))
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Data.query.get(int(user_id))
-
+#define user login form
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=3, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=3, max=30)])
     remember = BooleanField('remember me')
 
+#define user registration form
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email')])
     username = StringField('username', validators=[InputRequired(), Length(min=3, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=3, max=30)])
     company = StringField('company', validators=[InputRequired()])
 
+@login_manager.user_loader
+def load_user(user_id):
+    return storeUserData.query.get(int(user_id))
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+'''verifying password match with module login_user of flask_login library and
+in case of success redirect to a bootstrap dashboard page'''
+
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-
     if form.validate_on_submit():
-        user = Data.query.filter_by(username=form.username.data).first()
+        user = storeUserData.query.filter_by(username=form.username.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user,remember=form.remember.data)
                 return redirect(url_for('dashboard'))
-
         return '<h1>Invalid username or password.</h1>'
     return render_template('login.html', form=form)
+
+'''adding new_user in to database with a generated hash password
+(used werkzeug.security library) so you don't see the password in clear, just in
+case someone queries the database'''
 
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     form = RegisterForm()
-
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = Data(username=form.username.data, password=hashed_password, email=form.email.data, company=form.company.data)
+        new_user = storeUserData(username=form.username.data, password=hashed_password,
+        email=form.email.data, company=form.company.data)
         db.session.add(new_user)
         db.session.commit()
         return '<h1> New user has been created.</h1>'
-
     return render_template('signup.html', form=form)
 
 @app.route("/logout")
-@login_required
+@login_required #using module login_required to verify if you are logged in or not
 def logout():
     logout_user()
     return redirect(url_for('index'))
